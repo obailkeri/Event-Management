9ada292ec250e13dc4c85c884f3f5ea39dcf932b
Atharva Joshi <athu200299@gmail.com>
2019-01-25 11:49:55 +0530

    PHP Conversion

--- Login Page/long_page.php
+++ Login Page/long_page.php
@@ -1,8 +1,25 @@
+<?php
+
+start_session();
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+{
+	header("location: testlogin.php");
+	exit;
+}
+
+require_once "config.php";
+
+$username = $password = "";
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Login Form</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="login_style.css">
 </head>
   <body>
 <form method="post" action="page1.html">
